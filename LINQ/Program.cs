be344e9b5ac244028684f74f0b48fc5e0a4f3de7
Alec <alec.m.hancock@gmail.com>
2023-03-28 09:28:35 -0500

    Assignment Completed.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace LINQ
 {
     public class Program
     {
+        //Create a list of video game names...
+        //Order the list of games by length of the game name.
+        //Use the lambda expression in this exercise as well.
+        //Use Method Syntax for this exercise
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            List<string> videogames = new List<string>();
+
+            videogames.Add("Halo");
+            videogames.Add("Portal");
+            videogames.Add("Warcraft");
+            videogames.Add("Civilization");
+            videogames.Add("skate.");
+
+            var answer = videogames.Where(item => item.Length > 0).OrderBy(item => item).ToList();
+            for (int i = 0; i < answer.Count; i++)
+            {
+                Console.WriteLine(answer[i]);
+            }
+
         }
     }
 }
